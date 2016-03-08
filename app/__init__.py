e1feb23c3c8f878fe304416a049bb1d15ff2964e
cisko <8arcena@gmail.com>
2016-03-08 13:50:51 -0800

    Create new files. Implement form submission

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from forms import ApplicationForm
 
 def create_app():
 	app = Flask(__name__)
-
+	app.config['SECRET_KEY'] = 'our_secret_key'
+	app.config['CSRF_SESSION_KEY'] = 'THIS IS THE OTHER KEY'
 	@app.route('/')
 	def index():
 		return render_template('index.html')
@@ -10,6 +12,13 @@ def create_app():
 	@app.route('/login')
 	def login():
 		return "LOGIN PAGE"
+
+	@app.route('/apply',methods=['GET','POST'])
+	def apply():
+		form = ApplicationForm(request.form)
+		if form.validate_on_submit():
+			return 'Thanks for applying!'
+		return render_template('application.html', form = form)
 
 	return app
 

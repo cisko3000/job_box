d5434b8bd3cbf8109c68de05b9e9e9fe3e603a3a
cisko <8arcena@gmail.com>
2016-03-11 15:36:38 -0800

    Fix conflicts

--- app/forms.py
+++ app/forms.py
@@ -1,23 +1,14 @@
 from flask.ext.wtf import Form
-<<<<<<< HEAD
-from wtforms.fields import TextField, TextAreaField, FileField, SubmitField
+
 from wtforms.validators import Required
-=======
 from wtforms import TextField, FileField, SubmitField
->>>>>>> e1feb23c3c8f878fe304416a049bb1d15ff2964e
+
+
 
 class ApplicationForm(Form):
 	first_name = TextField("First Name")
 	last_name = TextField("Last Name")
-<<<<<<< HEAD
 	email = TextField("Email", [Required()])
 	resume = FileField("Resume")
 
 	submit = SubmitField("Apply")
-
-=======
-	email = TextField("Email")
-	resume = FileField("Resume")
-
-	submit = SubmitField('Apply')
->>>>>>> e1feb23c3c8f878fe304416a049bb1d15ff2964e

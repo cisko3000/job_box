abc3f9da51bd1bdc00b197559073146dc10f170e
cisko <8arcena@gmail.com>
2016-03-18 16:14:47 -0700

    Implement file upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from forms import ApplicationForm
+
+from werkzeug import secure_filename
 
 db = SQLAlchemy()
 
@@ -10,11 +13,19 @@ def create_app():
 	app.config['CSRF_SESSION_KEY'] = 'This is our secret key'
 	app.config['SECRET_KEY'] = "cookie_secret_key"
 	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/job_box.db'
+	app.config['UPLOAD_FOLDER'] = '/home/cisko/sites/job_box/app/resumes'
+	app.config['ALLOWED_FILE_EXTENSIONS'] =set(['txt','pdf','doc','docx'])
 
 	from models import Application
 
 	db.init_app(app)
 
+
+	def allowed_file(fname):
+		if '.' in fname and fname.rsplit('.',1)[1] in app.config['ALLOWED_FILE_EXTENSIONS']:
+			return True
+		else:
+			return False
 	@app.route('/')
 	def index():
 		return render_template('index.html')
@@ -27,12 +38,19 @@ def create_app():
 	def apply():
 		form = ApplicationForm()
 		if form.validate_on_submit():
-			new_applicant = Application()
-			form.populate_obj(new_applicant)
-			new_applicant.resume = ''
-			db.session.add(new_applicant)
-			db.session.commit()
-			return "Thanks for applying!"
+			file = request.files['resume']
+			if file and allowed_file(file.filename):
+				filename = secure_filename(file.filename)
+				file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+				new_applicant = Application()
+				form.populate_obj(new_applicant)
+				new_applicant.resume = filename
+				db.session.add(new_applicant)
+				db.session.commit()
+				return "Thanks for applying!"
+			else:
+				flash("Invalid file extension")
+				return render_template('application.html', form=form)		
 		return render_template('application.html', form=form)
 	return app
 
